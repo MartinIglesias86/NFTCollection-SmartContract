fb55bee41eb578764a87b23ee366dad2fd6b660f
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-05-19 12:30:32 -0300

    updated /constants/index.js

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x8e4E5184c20Bb65415234d077AdEAAb71A6fBA7b";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nftc-ollection-d-app.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
